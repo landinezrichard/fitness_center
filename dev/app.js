b3ecb46b5c7d6b6ba59be8da5c6ce70704ae7008
Ricardo <landinezrichard@gmail.com>
2016-02-09 21:45:46 -0500

    Listos estilos header y menú

---
+++ dev/app.js
@@ -0,0 +1,13 @@
+(function () {
+	/* Dependencias*/
+	
+	const showMenu = require('./components/MainMenu');
+
+	/*Listeners*/
+	document.addEventListener('DOMContentLoaded', onDOMload);
+
+	function onDOMload() {		
+		showMenu.init();		
+	}
+
+}())

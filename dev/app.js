0bd6a1cada825f38ece5f2b5d53ad0a0e8c394fd
Ricardo <landinezrichard@gmail.com>
2016-02-10 22:40:57 -0500

    Listos estilos slider

--- dev/app.js
+++ dev/app.js
@@ -4,12 +4,27 @@
 	const showMenu = require('./components/MainMenu');
 	const slides   = require('./components/Slider');
 
+	const clase1 = 'icon-chevron-thin-left',
+		  clase2 = 'icon-chevron-thin-right';
+
 	/*Listeners*/
 	document.addEventListener('DOMContentLoaded', onDOMload);
 
+	function addPrevNextSlide () {
+		var prev = document.querySelector('.prev');
+		prev.classList.add(clase1);
+		prev.innerText = '';
+
+		var next = document.querySelector('.next');
+		next.classList.add(clase2);
+		next.innerText = '';
+		
+	}
+
 	function onDOMload() {		
 		showMenu.init();
-		slides.init();		
+		slides.init();
+		addPrevNextSlide ()	
 	}
 
 }())
